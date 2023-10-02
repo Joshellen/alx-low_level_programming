6d0d6696d79ab2f6fdf3e9c49abcbc5821c4a974
Joshellen <141355088+Joshellen@users.noreply.github.com>
2023-10-02 11:38:12 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * _memset - fills memory with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: bytes filled.
- * Return: the pointer to dest.
- */
+* _memset - fills memory with a constant byte
+* @s: pointer to memory
+* @b: byte to fill with
+* @n: number of bytes to be filled
+*
+* Return: returns pointer to memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
 	return (s);
 }
